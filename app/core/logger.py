b9a8af4880a444abd390701d06c554ff7d35e239
ajmal <ajmalpn.1007@gmail.com>
2026-02-01 22:55:12 +0530

    Implemented custom formatting in log

--- app/core/logger.py
+++ app/core/logger.py
@@ -11,10 +11,22 @@ LOG_DIR.mkdir(parents=True, exist_ok=True)
 
 LOG_FILE = LOG_DIR / f"{datetime.now().strftime('%Y-%m-%d')}.log"
 
-logger.remove()
+CUSTOM_FORMAT = (
+    "<level>{level: <5}</level> | "
+    "{extra[username]} | "
+    "{extra[tenant]} | "
+    "{module} | "
+    "{function} | "
+    "{message}"
+)
 
+# Set global default values so fields always exist
+logger.configure(extra={"username": "N/A", "tenant": "N/A"})
+
+logger.remove()
 logger.add(
     LOG_FILE,
+    format=CUSTOM_FORMAT,
     rotation=settings.log_rotation,         # New log file every day
     retention=settings.log_retention,      # Keep logs for 10 days
     compression=settings.log_compression,        # Compress old logs
